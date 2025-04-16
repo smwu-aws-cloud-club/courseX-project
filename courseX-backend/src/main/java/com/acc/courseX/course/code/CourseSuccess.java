e67afe1d9d6c24e88317094794112b34583998dc
sung-silver <jinna0319@gmail.com>
2025-04-17 03:14:11 +0900

    feat: 강의 목록 조회 성공에 대한 success code 구현

---
+++ courseX-backend/src/main/java/com/acc/courseX/course/code/CourseSuccess.java
@@ -0,0 +1,19 @@
+package com.acc.courseX.course.code;
+
+import static lombok.AccessLevel.PRIVATE;
+
+import com.acc.courseX.common.code.SuccessCode;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor(access = PRIVATE)
+public enum CourseSuccess implements SuccessCode {
+  // 200
+  GET_COURSE_LIST(HttpStatus.OK, "강의 목록 조회에 성공했습니다.");
+
+  private final HttpStatus status;
+  private final String message;
+}
