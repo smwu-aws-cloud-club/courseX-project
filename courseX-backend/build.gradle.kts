fe3f81b39438ac4a12f4fe3e63c8dd2931f9868e
sung-silver <jinna0319@gmail.com>
2025-04-16 19:01:01 +0900

    add: lombok, h2 database, jpa 의존성 추가

--- courseX-backend/build.gradle.kts
+++ courseX-backend/build.gradle.kts
@@ -19,8 +19,14 @@ repositories {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	compileOnly("org.projectlombok:lombok")
+	annotationProcessor("org.projectlombok:lombok")
+	runtimeOnly("com.h2database:h2")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+	testRuntimeOnly("com.h2database:h2")
 }
 
 tasks.withType<Test> {
