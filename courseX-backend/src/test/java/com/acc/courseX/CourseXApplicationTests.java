70602049ec8813207ffe3cf3a4b8f74f127206e9
sung-silver <jinna0319@gmail.com>
2025-04-17 01:36:49 +0900

    test: test 환경에서 env 주입

--- courseX-backend/src/test/java/com/acc/courseX/CourseXApplicationTests.java
+++ courseX-backend/src/test/java/com/acc/courseX/CourseXApplicationTests.java
@@ -2,8 +2,12 @@ package com.acc.courseX;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@ActiveProfiles("test")
+@TestPropertySource(locations = {"classpath:env/test.env"})
 class CourseXApplicationTests {
 
   @Test
