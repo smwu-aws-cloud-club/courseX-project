f3ce7aa5344ba3cce3ecfcde1140d34aaf401f66
sung-silver <jinna0319@gmail.com>
2025-04-16 19:10:53 +0900

    setting: pre-commit을 통한 spotless 환경설정

--- courseX-backend/build.gradle.kts
+++ courseX-backend/build.gradle.kts
@@ -2,10 +2,12 @@ plugins {
 	java
 	id("org.springframework.boot") version "3.4.4"
 	id("io.spring.dependency-management") version "1.1.7"
+	id ("com.diffplug.spotless") version "6.25.0"
 }
 
 group = "com.acc"
 version = "0.0.1-SNAPSHOT"
+val googleJavaFormatVersion = "1.18.1"
 
 java {
 	toolchain {
@@ -29,6 +31,30 @@ dependencies {
 	testRuntimeOnly("com.h2database:h2")
 }
 
+spotless{
+	java {
+		target("**/*.java")
+		googleJavaFormat(googleJavaFormatVersion)
+		importOrder("courseX", "java", "javax", "jakarta", "acc", "com")
+		endWithNewline()
+		removeUnusedImports()
+	}
+}
+
 tasks.withType<Test> {
 	useJUnitPlatform()
+}
+
+tasks.register<Copy>("updateGitHooks") {
+	from("./scripts/pre-commit")
+	into("../.git/hooks")
+}
+
+tasks.register<Exec>("makeGitHooksExecutable") {
+	commandLine("chmod", "+x", "../.git/hooks/pre-commit")
+	dependsOn("updateGitHooks")
+}
+
+tasks.named("compileJava") {
+	dependsOn("makeGitHooksExecutable")
 }
