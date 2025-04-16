91d3145423c5db99aca9af28b5a151aff94c69a2
sung-silver <jinna0319@gmail.com>
2025-04-17 02:52:20 +0900

    feat: 성공/실패 코드 interface 정의

---
+++ courseX-backend/src/main/java/com/acc/courseX/support/code/FailureCode.java
@@ -0,0 +1,3 @@
+package com.acc.courseX.support.code;
+
+public interface FailureCode extends BaseCode {}
---
+++ courseX-backend/src/main/java/com/acc/courseX/support/code/SuccessCode.java
@@ -0,0 +1,3 @@
+package com.acc.courseX.support.code;
+
+public interface SuccessCode extends BaseCode {}
