c0bee589374a369156f40a70691d7feebb49d5ec
sung-silver <jinna0319@gmail.com>
2025-04-17 02:42:38 +0900

    feat: Weekday에 DisplayName 추가

--- courseX-backend/src/main/java/com/acc/courseX/entity/enums/Weekday.java
+++ courseX-backend/src/main/java/com/acc/courseX/entity/enums/Weekday.java
@@ -1,9 +1,16 @@
 package com.acc.courseX.entity.enums;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum Weekday {
-  MON,
-  TUE,
-  WED,
-  THU,
-  FRI
+  MON("월"),
+  TUE("화"),
+  WED("수"),
+  THU("목"),
+  FRI("금");
+
+  private final String displayName;
 }
