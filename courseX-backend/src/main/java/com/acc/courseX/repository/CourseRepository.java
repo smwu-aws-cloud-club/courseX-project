e0fdb219be122b02c47763f75017958ac8b3008b
sung-silver <jinna0319@gmail.com>
2025-04-17 02:44:33 +0900

    feat: CourseRepository에 findByCode 구현

--- courseX-backend/src/main/java/com/acc/courseX/repository/CourseRepository.java
+++ courseX-backend/src/main/java/com/acc/courseX/repository/CourseRepository.java
@@ -1,7 +1,11 @@
 package com.acc.courseX.repository;
 
+import java.util.List;
+
 import com.acc.courseX.entity.Course;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CourseRepository extends JpaRepository<Course, Long> {}
+public interface CourseRepository extends JpaRepository<Course, Long> {
+  List<Course> findByCode(String code);
+}
