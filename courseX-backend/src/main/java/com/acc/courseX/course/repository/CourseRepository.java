5b5574f362b9de3e038d8735aae6d4d76e3c861a
sung-silver <jinna0319@gmail.com>
2025-04-19 01:18:27 +0900

    feat: course repository에 findByIdOrThrow() 구현

--- courseX-backend/src/main/java/com/acc/courseX/course/repository/CourseRepository.java
+++ courseX-backend/src/main/java/com/acc/courseX/course/repository/CourseRepository.java
@@ -1,11 +1,18 @@
 package com.acc.courseX.course.repository;
 
+import static com.acc.courseX.course.code.CourseFailure.NOT_FOUND_COURSE;
+
 import java.util.List;
 
 import com.acc.courseX.course.entity.Course;
+import com.acc.courseX.course.exception.CourseException;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CourseRepository extends JpaRepository<Course, Long> {
   List<Course> findByCode(String code);
+
+  default Course findByIdOrThrow(Long courseId) {
+    return findById(courseId).orElseThrow(() -> new CourseException(NOT_FOUND_COURSE));
+  }
 }
