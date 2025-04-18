50dcf5a5d9bcea4e017dc68d94fec9f6a0ca3006
sung-silver <jinna0319@gmail.com>
2025-04-19 00:14:34 +0900

    feat: 전공강의 검증에 대한 MajorCourseEnrollmentValidator 구현

---
+++ courseX-backend/src/main/java/com/acc/courseX/enrollment/validator/MajorCourseEnrollmentValidator.java
@@ -0,0 +1,27 @@
+package com.acc.courseX.enrollment.validator;
+
+import static com.acc.courseX.enrollment.code.EnrollmentFailure.MAJOR_MISMATCH;
+
+import com.acc.courseX.course.entity.Course;
+import com.acc.courseX.enrollment.exception.EnrollmentException;
+import com.acc.courseX.enrollment.repository.EnrollmentRepository;
+import com.acc.courseX.user.entity.User;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class MajorCourseEnrollmentValidator extends AbstractEnrollmentValidator {
+
+  public MajorCourseEnrollmentValidator(EnrollmentRepository enrollmentRepository) {
+    super(enrollmentRepository);
+  }
+
+  @Override
+  protected void validateCourseSpecificRules(Course course, User user) {
+    boolean isMajorMismatch = !course.getProfessor().getMajor().equals(user.getMajor());
+
+    if (isMajorMismatch) {
+      throw new EnrollmentException(MAJOR_MISMATCH);
+    }
+  }
+}
