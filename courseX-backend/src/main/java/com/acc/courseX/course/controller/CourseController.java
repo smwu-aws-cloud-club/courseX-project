3f198e29fceb104b5baf817e499b6a88cdb95840
sung-silver <jinna0319@gmail.com>
2025-04-18 21:36:30 +0900

    feat: 수강신청 API 인터페이스 정의

--- courseX-backend/src/main/java/com/acc/courseX/course/controller/CourseController.java
+++ courseX-backend/src/main/java/com/acc/courseX/course/controller/CourseController.java
@@ -11,6 +11,8 @@ import com.acc.courseX.course.service.CourseService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,5 +28,11 @@ public class CourseController implements CourseApi {
   public ResponseEntity<?> getCourses(@RequestParam(required = false) final String code) {
     List<CourseResponse> response = courseService.getCourses(code);
     return ResponseUtil.success(GET_COURSE_LIST, response);
+  }
+
+  @PostMapping("/{courseId}/enroll")
+  @Override
+  public ResponseEntity<?> enroll(@PathVariable Long courseId, Long userId) {
+    return null;
   }
 }
