3c660d2241d750d2faf0ce05fc8bdba1a8810935
haesa <xdhaesa@gmail.com>
2025-04-20 20:20:52 +0900

    chore: axios 및 react-query 의존성 추가(#23)

---
+++ course-x-frontend/src/axios.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+export const defaultAxios = axios.create({
+  baseURL: process.env.REACT_APP_SERVER_DOMAIN,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+export const authAxios = axios.create({
+  baseURL: process.env.REACT_APP_SERVER_DOMAIN,
+  headers: {
+    'Content-Type': 'application/json',
+    withCredentials: true,
+  },
+});
--- course-x-frontend/src/index.js
+++ course-x-frontend/src/index.js
@@ -3,15 +3,20 @@ import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { RouterProvider, createBrowserRouter } from 'react-router';
 import { routes } from 'route';
 
 const root = document.getElementById('root');
 const router = createBrowserRouter(routes);
 
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(root).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </React.StrictMode>
 );
 
