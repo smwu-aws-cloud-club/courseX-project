dbbc56d90d402580e715f8589326dce6f3de0726
sung-silver <jinna0319@gmail.com>
2025-04-22 21:15:09 +0900

    feat: 템플릿 메서드 생성자에 CourseScheduleRepository도 포함되도록 변경

--- courseX-backend/src/main/java/com/acc/courseX/enrollment/validator/MajorCourseEnrollmentValidator.java
+++ courseX-backend/src/main/java/com/acc/courseX/enrollment/validator/MajorCourseEnrollmentValidator.java
@@ -3,6 +3,7 @@ package com.acc.courseX.enrollment.validator;
 import static com.acc.courseX.enrollment.code.EnrollmentFailure.MAJOR_MISMATCH;
 
 import com.acc.courseX.course.entity.Course;
+import com.acc.courseX.course.repository.CourseScheduleRepository;
 import com.acc.courseX.enrollment.exception.EnrollmentException;
 import com.acc.courseX.enrollment.repository.EnrollmentRepository;
 import com.acc.courseX.user.entity.User;
@@ -12,8 +13,10 @@ import org.springframework.stereotype.Component;
 @Component
 public class MajorCourseEnrollmentValidator extends AbstractEnrollmentValidator {
 
-  public MajorCourseEnrollmentValidator(EnrollmentRepository enrollmentRepository) {
-    super(enrollmentRepository);
+  public MajorCourseEnrollmentValidator(
+      EnrollmentRepository enrollmentRepository,
+      CourseScheduleRepository courseScheduleRepository) {
+    super(enrollmentRepository, courseScheduleRepository);
   }
 
   @Override
