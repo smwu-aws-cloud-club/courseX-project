625eca61cc2d26ea9a08cbc3816e52e148fe4327
haesa <xdhaesa@gmail.com>
2025-04-23 17:16:32 +0900

    feat: 수강 신청 내역 조회 API 연결(#25)

--- course-x-frontend/src/page/CancelPage.jsx
+++ course-x-frontend/src/page/CancelPage.jsx
@@ -1,10 +1,45 @@
+import { useQuery } from '@tanstack/react-query';
+
+import { fetchEnrollments } from 'api/course';
+
 import Button from 'components/button';
-import course from 'mock/course.json';
+
 import info from 'mock/info.json';
 import style from './CancelPage.module.css';
 
+function Row({
+  enrollmentId,
+  courseCode,
+  courseName,
+  courseCredit,
+  courseProfessorName,
+  courseSchedule,
+}) {
+  return (
+    <div className={style.row}>
+      <div className={style.cell}>{courseCode}</div>
+      <div className={style.cell}>{courseName}</div>
+      <div className={style.cell}>{courseCredit}</div>
+      <div className={style.cell}>{courseProfessorName}</div>
+      <div className={style.cell}>
+        {courseSchedule
+          .split(',')
+          .map((str) => str.trim())
+          .join(`\n`)}
+      </div>
+      <div className={style.cell}>
+        <Button onClick={() => alert(`${courseName} 신청 취소`)}>취소</Button>
+      </div>
+    </div>
+  );
+}
+
 export default function CancelPage() {
   const { name, totalCredit } = info;
+  const { data: courses, isLoading } = useQuery({
+    queryKey: ['enrollments'],
+    queryFn: fetchEnrollments,
+  });
 
   return (
     <section className={style.container}>
@@ -13,27 +48,23 @@ export default function CancelPage() {
         <span className={style.strong}>{totalCredit}</span> 학점입니다.
       </div>
       <div className={style.view_container}>
-        <div className={`${style.row} ${style.header}`}>
-          <div className={style.cell}>과목번호</div>
-          <div className={style.cell}>과목명</div>
-          <div className={style.cell}>학점</div>
-          <div className={style.cell}>교수님</div>
-          <div className={style.cell}>강의시간</div>
-          <div className={style.cell}>신청</div>
-        </div>
-
-        {course.map(({ code, name, credit, professorName, courseSchedule }) => (
-          <div key={code} className={style.row}>
-            <div className={style.cell}>{code}</div>
-            <div className={style.cell}>{name}</div>
-            <div className={style.cell}>{credit}</div>
-            <div className={style.cell}>{professorName}</div>
-            <div className={style.cell}>{courseSchedule}</div>
-            <div className={style.cell}>
-              <Button onClick={() => alert(`${name} 신청 취소`)}>취소</Button>
+        {isLoading && <div>로딩 중...</div>}
+        {!isLoading && (
+          <>
+            <div className={`${style.row} ${style.header}`}>
+              <div className={style.cell}>과목번호</div>
+              <div className={style.cell}>과목명</div>
+              <div className={style.cell}>학점</div>
+              <div className={style.cell}>교수님</div>
+              <div className={style.cell}>강의시간</div>
+              <div className={style.cell}>신청</div>
             </div>
-          </div>
-        ))}
+
+            {courses.map((course) => (
+              <Row key={course.code} {...course} />
+            ))}
+          </>
+        )}
       </div>
     </section>
   );
