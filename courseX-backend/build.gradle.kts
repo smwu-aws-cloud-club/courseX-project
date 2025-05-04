8aacc21afeab9d79a8b7184f8b7dfd06e6e1839d
sung-silver <jinna0319@gmail.com>
2025-05-04 21:26:05 +0900

    add: actuator 의존성 추가

--- courseX-backend/build.gradle.kts
+++ courseX-backend/build.gradle.kts
@@ -28,6 +28,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("org.springframework.boot:spring-boot-starter-aop")
 	implementation("io.micrometer:micrometer-registry-prometheus")
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
 
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
